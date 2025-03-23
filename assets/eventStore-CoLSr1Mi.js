90401d8db05fac36188af194d8694a7356060ba1
vettel-2024 <vettel-2024@users.noreply.github.com>
2025-03-23 09:34:25 +0000

    deploy: 941fbe24c45b477f5261de49ce13bee5328c0ddc

---
+++ assets/eventStore-CoLSr1Mi.js
@@ -0,0 +1,2 @@
+import{g as t}from"./index-i6x426Ic.js";const n=t("eventStore",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvents(){return this.events}}});export{n as u};
+//# sourceMappingURL=eventStore-CoLSr1Mi.js.map
